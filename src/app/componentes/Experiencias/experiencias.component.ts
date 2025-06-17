0cca3375c4b6161747f367eb7ce57a1ba6f5b574
Pedro Felipe <pedrofelipeot@gmail.com>
2025-06-17 16:36:47 -0300

    Ajustes de fundos animados

--- src/app/componentes/Experiencias/experiencias.component.ts
+++ src/app/componentes/Experiencias/experiencias.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+
+declare var VANTA: any;
+declare var ScrollReveal: any;
+declare var THREE: any;
 
 @Component({
   selector: 'app-experiencias',
@@ -6,6 +10,28 @@ import { Component } from '@angular/core';
   templateUrl: './experiencias.component.html',
   styleUrl: './experiencias.component.css'
 })
-export class ExperienciasComponent {
+export class ExperienciasComponent implements AfterViewInit, OnDestroy {
+  private vantaEffect: any;
 
+  ngAfterViewInit(): void {
+    this.vantaEffect = VANTA.GLOBE({
+      el: document.querySelector('.fundo-experiencia'),
+      THREE: THREE,
+      color: 0x00b7ff,
+      backgroundColor: 0x01a1a2e,
+      size: 1.2
+    });
+
+    ScrollReveal().reveal('.fade-in-up', {
+      distance: '40px',
+      duration: 800,
+      easing: 'ease-out',
+      origin: 'bottom',
+      interval: 150
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.vantaEffect) this.vantaEffect.destroy();
+  }
 }
